54b6455315e7acecefb427a3210315e02e45620b
sowa <106477529+sowa-remon@users.noreply.github.com>
2023-05-21 17:41:09 -0600

    ya lo ultimo de hoy

---
+++ src/js/tipoPension.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var tiposPension = document.getElementById("opciones");
+  
+    tiposPension.addEventListener("change", function() {
+      var pensionSeleccionada = tiposPension.value;
+      console.log("Pensión seleccionada: " + pensionSeleccionada);
+    });
+  });
+  
