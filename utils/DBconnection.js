8bc935b2b32e1b3c29e59f2a022a6e45c4767707
SrCosmicCat <gatitosplus000@gmail.com>
2024-11-20 20:11:02 -0600

    refactor: simplify Sequelize initialization in DB connection

--- utils/DBconnection.js
+++ utils/DBconnection.js
@@ -6,7 +6,7 @@ dotenv.config();
 const dbName = process.env.NODE_ENV === 'test' ? process.env.DB_TEST_NAME : process.env.DB_NAME;
 console.log(dbName);
 
-const sequelize = new Sequelize(dbName, process.env.DB_USER, process.env.DB_PASS, {
+const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
