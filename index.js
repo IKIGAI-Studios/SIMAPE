b2e09f94c207d91f4fe0b0fe4c371afcf5aac1fa
SrCosmicCat <gatitosplus000@gmail.com>
2024-11-22 12:05:05 -0600

    sessions test

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ import { socketsUsuario } from './sockets/socketsUsuario.js';
 import delegacionRoutes from './routes/delegacionRoutes.js';
 import movimientoRoutes from './routes/movimientoRoutes.js';
 import peticionRoutes from './routes/peticionRoutes.js';
+//import { Sequelize } from 'sequelize';
+//import sequelize from './utils/DBconnection.js';
+//import SequelizeStore from 'connect-session-sequelize';
 
 // Declarar constantes para uso de rutas estaticas
 const __filename = fileURLToPath(import.meta.url);
@@ -29,6 +32,7 @@ const PORT = process.env.PORT || 6969;
 const app = express();
 const HTTPserver = http.createServer(app);
 const io = new Server(HTTPserver);
+//const SequelizeSessionStore = SequelizeStore(session.Store);
 
 // Sockets
 socketsUsuario(io);
@@ -41,13 +45,18 @@ app.use(nocache());
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 app.use(cookieParser());
+
+// const sessionStore = new SequelizeSessionStore({
+//   db: sequelize,
+// });
+
 app.use(session({
   secret: process.env.SECRET_KEY_SESSION,
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   cookie: { 
-    secure: process.env.NODE_ENV === 'production', // Solo en HTTPS
-    httpOnly: true,
+    //secure: process.env.NODE_ENV === 'production', // Solo en HTTPS
+    //httpOnly: true,
     maxAge: 1000 * 60 * 60 * 12 
   }
 }));
